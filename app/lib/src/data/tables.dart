55d6c7c514d21d9750e1c20149b0848843a5ebd1
Peter Kolloch <info@eigenvalue.net>
2023-05-13 21:10:56 +0200

    [drift] trying to get it working on web -- but not yet

--- app/lib/src/data/tables.dart
+++ app/lib/src/data/tables.dart
@@ -1,10 +1,8 @@
 import 'package:drift/drift.dart';
-import 'dart:io';
 
-import 'package:drift/native.dart';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' as p;
+import 'package:robokru/connection/connection.dart';
 import 'package:robokru/src/data/query/locations.dart';
 import 'package:robokru/src/data/query/projects.dart';
 import 'package:robokru/src/data/query/scenes.dart';
@@ -77,9 +75,7 @@ class SkaiDb extends _$SkaiDb {
 
   static QueryExecutor _openConnection() {
     return LazyDatabase(() async {
-      final dbFolder = await getApplicationDocumentsDirectory();
-      final file = File(p.join(dbFolder.path, 'skaidb.sqlite'));
-      return NativeDatabase.createInBackground(file);
+      return connect();
     });
   }
 
